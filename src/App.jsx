8666a5cac86c07b5f095473ab3f9bfa24124ad5d
Nisarg <nisarganand541@gmail.com>
2025-08-01 08:03:05 +0530

    theme functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import React from 'react'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './Components/ThemeBtn';
+import Card from './Components/Card';
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [themeMode,setThemeMode]=useState(null);
+
+  const darkTheme=()=>{
+    setThemeMode("dark");
+  }
+  const lightTheme=()=>{
+    setThemeMode("light");
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light","dark");
+    document.querySelector('html').classList.add(themeMode);
+  }, [themeMode])
+  
 
   return (
-    <>
+    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
       
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* themeBtn */}
+                       <ThemeBtn/>
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       {/* card */}
+                    <Card/>
                     </div>
                 </div>
             </div>
 
-    </>
+    </ThemeProvider>
   )
 }
 
